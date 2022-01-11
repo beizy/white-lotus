57d719d0ba5b87c2794d20b274b184132b967e08
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2022-01-10 20:49:56 -0500

    Clean up comments, console.log. Update api address

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,30 +1,30 @@
 /* eslint-disable max-len */
 
 function getAllCustomers() {
-  return fetch("http://localhost:3001/api/v1/customers")
+  return fetch("http://overlookdata.herokuapp.com/api/v1/customers")
     .then(response => response.json())
     .then(data => data.customers)
-    .catch(reason => console.log(reason));
+    .catch(reason => console.log(reason))
 }
 
 function getOneCustomer(id) {
-  return fetch(`http://localhost:3001/api/v1/customers/${id}`).then(response => response.json());
+  return fetch(`http://overlookdata.herokuapp.com/api/v1/customers/${id}`).then(response => response.json())
 }
 
 function getAllRooms() {
-  return fetch("http://localhost:3001/api/v1/rooms")
+  return fetch("http://overlookdata.herokuapp.com/api/v1/rooms")
     .then(response => response.json())
-    .then(data => data.rooms);
+    .then(data => data.rooms)
 }
 
 function getAllBookings() {
-  return fetch("http://localhost:3001/api/v1/bookings")
+  return fetch("http://overlookdata.herokuapp.com/api/v1/bookings")
     .then(response => response.json())
-    .then(data => data.bookings);
+    .then(data => data.bookings)
 }
 
 function addBooking(customerId, date, roomNumber) {
-  return fetch("http://localhost:3001/api/v1/bookings", {
+  return fetch("http://overlookdata.herokuapp.com/api/v1/bookings", {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({
@@ -32,20 +32,22 @@ function addBooking(customerId, date, roomNumber) {
       date: date,
       roomNumber: roomNumber,
     }),
-  }).then(response => {
-    if (!response.ok){
-      throw new Error (`The following technical issue occured: ${response.status} ${response.status.text}`)
-    }
-    return response.json()
-  }).catch(reason => alert(reason))
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`The following technical issue occured: ${response.status} ${response.status.text}`)
+      }
+      return response.json()
+    })
+    .catch(reason => alert(reason))
   // .then(data => console.log(data));
 }
 
 function deleteBooking(bookingId) {
-  fetch(`http://localhost:3001/api/v1/bookings/${bookingId}`, {
+  fetch(`http://overlookdata.herokuapp.com/api/v1/bookings/${bookingId}`, {
     method: "DELETE",
     headers: { "content-type": "application/json" },
-  }).then(response => console.log(response));
+  }).then(response => console.log(response))
 }
 
-export { getAllCustomers, getOneCustomer, getAllRooms, getAllBookings, addBooking, deleteBooking };
+export { getAllCustomers, getOneCustomer, getAllRooms, getAllBookings, addBooking, deleteBooking }
